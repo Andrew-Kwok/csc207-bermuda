e0ba727cdfd754d5d31a0916354ec2be69ae1fd1
laura1945 <66044726+laura1945@users.noreply.github.com>
2023-11-08 16:32:25 -0500

    did AddTaskController.java

--- src/main/java/interface_adapter/task/add_task/AddTaskController.java
+++ src/main/java/interface_adapter/task/add_task/AddTaskController.java
@@ -1,10 +1,21 @@
 package interface_adapter.task.add_task;
 
 import domains.task.use_case.add_task.AddTaskInputBoundary;
+import domains.task.use_case.add_task.AddTaskInputData;
 import domains.user.use_case.login.LoginInputBoundary;
 
-public class AddTaskController {
-    public AddTaskController(AddTaskInputBoundary addTaskUseCaseInteractor){
+import java.time.LocalDateTime;
 
+public class AddTaskController {
+
+    final AddTaskInputBoundary addTaskUseCaseInteractor;
+    public AddTaskController(AddTaskInputBoundary addTaskUseCaseInteractor){
+        this.addTaskUseCaseInteractor = addTaskUseCaseInteractor;
     }
+
+    public void execute(String taskName, String description, LocalDateTime deadline){
+        AddTaskInputData addTaskInputData = new AddTaskInputData(taskName, description, deadline);
+        addTaskUseCaseInteractor.execute(addTaskInputData);
+    }
+
 }
