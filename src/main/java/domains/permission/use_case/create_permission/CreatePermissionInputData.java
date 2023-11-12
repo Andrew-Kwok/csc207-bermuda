b9ce3ebcbacb3536085532e35a96096952bfa67d
Andrew <52167672+Andrew-Kwok@users.noreply.github.com>
2023-11-12 12:55:52 -0500

    Andrew permission view (#15)
    * add get-permission from cloudsql
    
    * move file to maven structure
    
    * make CRUD permission
    
    * add use cases for permissions
    
    * add getPermissionView
    
    * finalize createPermissionView & getPermissionView

--- src/main/java/domains/permission/use_case/create_permission/CreatePermissionInputData.java
+++ src/main/java/domains/permission/use_case/create_permission/CreatePermissionInputData.java
@@ -3,13 +3,30 @@ package domains.permission.use_case.create_permission;
 import domains.permission.entity.Permission;
 
 public class CreatePermissionInputData {
-    private final Permission permission;
-
-    public CreatePermissionInputData(Permission permission) {
-        this.permission = permission;
+    private final String userId;
+    private final String projectId;
+    private final String permissionName;
+    private final String permissionDescription;
+    public CreatePermissionInputData(String userId, String projectId, String permissionName, String permissionDescription) {
+        this.userId = userId;
+        this.projectId = projectId;
+        this.permissionName = permissionName;
+        this.permissionDescription = permissionDescription;
     }
 
-    public Permission getPermission() {
-        return permission;
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public String getPermissionName() {
+        return permissionName;
+    }
+
+    public String getPermissionDescription() {
+        return permissionDescription;
     }
 }
---
+++ src/main/java/view/GetPermissionView.java
@@ -0,0 +1,88 @@
+package view;
+
+import domains.permission.entity.Permission;
+import interface_adapter.permission.create_permission.CreatePermissionController;
+import interface_adapter.permission.create_permission.CreatePermissionViewModel;
+import interface_adapter.permission.get_permission.GetPermissionController;
+import interface_adapter.permission.get_permission.GetPermissionState;
+import interface_adapter.permission.get_permission.GetPermissionViewModel;
+import interface_adapter.view_model.ViewManagerModel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class GetPermissionView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "GetPermissionView";
+    private final GetPermissionViewModel getPermissionViewModel;
+    private final GetPermissionController getPermissionController;
+    private final CreatePermissionViewModel createPermissionViewModel;
+    private final CreatePermissionController createPermissionController;
+    private final ViewManagerModel viewManagerModel;
+
+    JLabel title;
+
+    final JButton createPermission;
+
+    DefaultListModel<Permission> permissionListModel = new DefaultListModel<>();
+
+    public GetPermissionView(ViewManagerModel viewManagerModel, GetPermissionViewModel getPermissionViewModel, GetPermissionController getPermissionController,
+                             CreatePermissionViewModel createPermissionViewModel, CreatePermissionController createPermissionController) {
+        this.viewManagerModel = viewManagerModel;
+        this.getPermissionViewModel = getPermissionViewModel;
+        this.getPermissionController = getPermissionController;
+        this.createPermissionViewModel = createPermissionViewModel;
+        this.createPermissionController = createPermissionController;
+        this.getPermissionViewModel.addPropertyChangeListener(this);
+
+        title = new JLabel(getPermissionViewModel.TITLE_LABEL);
+
+        // On initialization, get the permissions.
+        this.getPermissionController.execute(null);
+
+        JPanel buttons = new JPanel();
+
+        createPermission = new JButton(GetPermissionViewModel.CREATE_PERMISSION_BUTTON_LABEL);
+        buttons.add(createPermission);
+
+        createPermission.addActionListener(
+                new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(java.awt.event.ActionEvent evt) {
+                        if (evt.getSource().equals(createPermission)) {
+                            viewManagerModel.setActiveView(createPermissionViewModel.getViewName());
+                            viewManagerModel.firePropertyChanged();
+                        }
+                    }
+                }
+        );
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(title);
+        this.add(new JScrollPane(new JList<>(permissionListModel)));
+        this.add(buttons);
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        JOptionPane.showConfirmDialog(this, "Not implemented yet.");
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("getPermissionState")) {
+            GetPermissionState state = (GetPermissionState) evt.getNewValue();
+            if (state.getGetPermissionError() != null) {
+                JOptionPane.showMessageDialog(this, state.getGetPermissionError());
+            } else {
+                permissionListModel.clear();
+                for (Permission permission : state.getPermissions()) {
+                    permissionListModel.addElement(permission);
+                }
+            }
+        }
+    }
+}
