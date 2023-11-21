72a6151e7fe07e5cccfccced50c090acc70c9598
lauraz25 <laurazhan21@gmail.com>
2023-11-20 20:05:18 -0500

    .

--- src/main/java/domains/task/use_case/edit_task/EditTaskInputBoundary.java
+++ src/main/java/domains/task/use_case/edit_task/EditTaskInputBoundary.java
@@ -1,4 +1,5 @@
 package domains.task.use_case.edit_task;
 
 public interface EditTaskInputBoundary {
+    void execute(EditTaskInputData editTaskInputData);
 }
--- src/main/java/interface_adapter/task/get_task/GetTaskViewModel.java
+++ src/main/java/interface_adapter/task/get_task/GetTaskViewModel.java
@@ -1,4 +1,35 @@
 package interface_adapter.task.get_task;
 
+import interface_adapter.permission.get_permission.GetPermissionState;
+
+import java.beans.PropertyChangeSupport;
+
+import static constant.ViewConstant.GET_TASK_VIEW_NAME;
+
 public class GetTaskViewModel {
+    public final String TITLE_LABEL = "Get Task";
+    private GetTaskState state = new GetTaskState();
+    public static final String GET_TASK_BUTTON_LABEL = "Get Task";
+
+    public GetTaskViewModel() {
+        super();
+    }
+
+    public void setState(GetTaskState state) {
+        this.state = state;
+    }
+
+    public GetTaskState getState() {
+        return state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("getTaskState", null, this.state);
+    }
+
+    public void addPropertyChangeListener(java.beans.PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
