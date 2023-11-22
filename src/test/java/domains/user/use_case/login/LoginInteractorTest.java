60b4c24204a6df8a5f1d3d94387623f3a43f8924
HW.M1 <HW@M1.com>
2023-11-22 16:51:17 -0500

    initiate unitest for log in

---
+++ src/test/java/domains/user/use_case/login/LoginInteractorTest.java
@@ -0,0 +1,18 @@
+package domains.user.use_case.login;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginInteractorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void execute() {
+    }
+}
