e6e426ab68e1ed4885bb359cc635431b4889baff
laura1945 <66044726+laura1945@users.noreply.github.com>
2023-12-06 01:49:47 -0500

    addTaskInteractorTest not working

---
+++ src/test/domains/task/use_case/add_task/InMemoryDAO.java
@@ -0,0 +1,35 @@
+package data_access;
+
+import domains.task.entity.Task;
+import domains.task.use_case.add_task.AddTaskDataAccessInterface;
+import domains.task.use_case.close_task.CloseTaskDataAccessInterface;
+import domains.task.use_case.edit_task.EditTaskDataAccessInterface;
+import domains.task.use_case.get_task.GetTaskDataAccessInterface;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class InMemoryDAO implements AddTaskDataAccessInterface, GetTaskDataAccessInterface, EditTaskDataAccessInterface, CloseTaskDataAccessInterface {
+    private HashMap<String, Task> tasks =  new HashMap<>();
+    private int taskID = 1000000000;
+    @Override
+    public void addTask(Task task) throws Exception {
+        tasks.put(Integer.toString(taskID), task);
+        taskID++;
+    }
+
+    @Override
+    public void closeTask(String taskID) throws Exception {
+
+    }
+
+    @Override
+    public void editTask(Task task) throws Exception {
+
+    }
+
+    @Override
+    public List<Task> getTasks(String projectID) throws Exception {
+        return null;
+    }
+}
